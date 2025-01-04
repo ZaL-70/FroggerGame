5a4388cc8c7b1980120a273877f43937b6e7b3e1
Subhaan Afzal <psysa19@exmail.nottingham.ac.uk>
2025-01-04 18:36:17 +0000

    refactored AnimalController actorInteraction() method to follow DIP

---
+++ src/main/java/uk/ac/nott/cs/comp2013/froggerApp/controller/player/interactionHandler/WetTurtleInteraction.java
@@ -0,0 +1,17 @@
+package uk.ac.nott.cs.comp2013.froggerApp.controller.player.interactionHandler;
+
+import uk.ac.nott.cs.comp2013.froggerApp.model.actors.level.WetTurtle;
+import uk.ac.nott.cs.comp2013.froggerApp.model.actors.player.Animal;
+
+public class WetTurtleInteraction implements ObjectInteractionHandler {
+    @Override
+    public void interact(Animal animal) {
+        var wetTurtles = animal.getIntersectingObjects(WetTurtle.class);
+        if (!wetTurtles.isEmpty()) {
+            WetTurtle wetTurtle = wetTurtles.get(0);
+            if (!(wetTurtle.isSunk())) {
+                animal.move(-1,0);
+            }
+        }
+    }
+}
